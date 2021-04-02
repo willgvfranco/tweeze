b323ff4523f82c35b8fea1041e608f70b54c2a09
William 'neus' Franco <williamgvfranco@gmail.com>
2021-04-02 12:42:25 -0300

    w: stackoverflow question solved models create-only

--- frontend/admin.py
+++ frontend/admin.py
@@ -6,5 +6,10 @@ from .models import Fonte
 class FonteAdmin(admin.ModelAdmin):
     list_display = ('id', 'news_source', 'source_url_global',
                     'ativo', 'modificado')
-    # readonly_fields = ['source_slug']
+    readonly_fields = ['source_slug']
     list_display_links = ('id', 'news_source')
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['source_slug']
+        return []
