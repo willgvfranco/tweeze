3ec2416db95dc3b02dd2cb9b2b312600b7687edc
William Franco <williamgvfranco@gmail.com>
2021-05-24 17:38:34 -0300

    views query multipla

--- web/dashboard/views.py
+++ web/dashboard/views.py
@@ -8,6 +8,10 @@ from .forms import ContatoForm, LoginForm, RegisterForm, RegisterCompanyForm, Po
 from django.urls import reverse_lazy
 from .models import GruposDePalavras
 import json
+from frontend.models import Noticia
+from django.db.models import Q
+from django.views.generic.list import ListView
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 
 class baseDashboard(TemplateView):
@@ -74,14 +78,101 @@ class StatisticsView(baseDashboard):
         return context
 
 
-class ReportsView(baseDashboard):
+class ReportsView(ListView):
     template_name = 'reports.html'
+    model = Noticia
+    paginate_by = 30
+    context_object_name = 'noticias_filtradas'
+    # negativas = self.request.POST.get('sendNegativeForm')
+
+    positivas = "Flamengo"
+
+    queryPositiva = "'"+positivas.replace(",", "' | '")+"'"
+
+    vector = SearchVector('title', 'description')
+    query = SearchQuery(queryPositiva)
+    queryset = Noticia.objects.filter(criado__range=[
+                                      "2021-05-23", "2021-05-24"]).annotate(search=vector).filter(search=query).order_by('-id')
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        # print(arrayPositivas)
+        # print(queryPositiva)
+
+        # qs = Noticia.objects.filter(criado__range=["2021-05-21", "2021-05-24"]).annotate(
+        #     search=vector).filter(search=query).order_by('-id')
+        # print(list(qs))
+        return qs
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(ReportsView, self).get_context_data(**kwargs)
+    #     grupos = GruposDePalavras.objects.filter(owner=self.request.user.id).values(
+    #         'id', 'positivas', 'negativas', 'owner', 'grupo')
+    #     context['grupos'] = list(grupos)
+    #     self.request.session['grupos'] = list(grupos)
+    #     print(grupos)
+    #     return context
+
+    # def post(self, request, *args, **kwargs):
+    #     grupoID = self.request.POST.get('group')
+    #     positivas = self.request.POST.get('sendPositiveForm')
+    #     negativas = self.request.POST.get('sendNegativeForm')
+
+    #     arrayPositivas = positivas.split(',')
+    #     arrayNegativas = negativas.split(',')
+
+    #     def get_queryset(self):
+    #         qs = super().get_queryset()
+    #         termo = self.request.GET.get('termo')
+
+    #         if not termo:
+    #             return qs
+    #         print(termo)
+    #         qs = qs.filter(
+    #             Q(title__icontains=arrayPositivas)
+    #         )
+
+
+class ReportsBusca(baseDashboard):
+    # template_name = 'busca.html'
+    model = Noticia
+    # paginate_by = 30
+    # context_object_name = 'noticias'
 
     def get_context_data(self, **kwargs):
-        context = super(ReportsView, self).get_context_data(**kwargs)
-        context['segment'] = 'reports'
+        context = super(ReportsBusca, self).get_context_data(**kwargs)
 
+        # qs = super().get_queryset()
+        # grupoID = self.request.POST.get('group')
+        # positivas = self.request.POST.get('sendPositiveForm')
+        positivas = "Flamengo,vasco,fluminense,botafogo"
+        # negativas = self.request.POST.get('sendNegativeForm')
+
+        arrayPositivas = positivas.split(',')
+
+        queryPositiva = "'"+positivas.replace(",", "' | '")+"'"
+        # for palavra in arrayPositivas:
+        #     queryPositiva.
+        # arrayNegativas = negativas.split(',').
+        vector = SearchVector('title', 'description')
+        query = SearchQuery(queryPositiva)
+        print(arrayPositivas)
+        print(queryPositiva)
+        news = Noticia.objects \
+            .filter(criado__range=["2021-05-21", "2021-05-24"]) \
+            .annotate(search=vector) \
+            .filter(search=query) \
+
+        print(list(news))
+        context['noticias_filtradas'] = list(news)
         return context
+        # if not arrayPositivas:
+        #     return qs
+        # qs = qs.filter(title__in=list(arrayPositivas)
+        #                )
+        # print(qs)
+        # return qs
 
 
 class CriarGrupoView(baseDashboard):
@@ -100,11 +191,11 @@ class CriarGrupoView(baseDashboard):
         arrayPositivas = positivas.split(',')
         arrayNegativas = negativas.split(',')
 
-        if(list(arrayPositivas)[0] == ''):
-            arrayPositivas = None
+        # if arrayPositivas[0] == '':
+        #     arrayPositivas = None
 
-        if(list(arrayNegativas)[0] == ''):
-            arrayPositivas = None
+        # if arrayNegativas[0] == '':
+        #     arrayPositivas = None
         # EDITANDO EXISTENTE
         if groupid:
             grupo_existente = GruposDePalavras.objects.filter(
@@ -147,10 +238,10 @@ class SearchTermsView(TemplateView):
         context = super(SearchTermsView, self).get_context_data(**kwargs)
         grupos = GruposDePalavras.objects.filter(owner=self.request.user.id).values(
             'id', 'positivas', 'negativas', 'owner', 'grupo')
+        context['grupos'] = list(grupos)
         print(grupos)
         context['segment'] = 'searchterms'
         context['iduser'] = self.request.user.id
-        context['grupos'] = list(grupos)
         return context
 
 
