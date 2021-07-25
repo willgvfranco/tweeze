279e4dd46605ae14ee84bc6588eefed356f2aa78
William Franco <williamgvfranco@gmail.com>
2021-07-25 00:32:17 -0300

    google wip

--- web/backend/settings/base.py
+++ web/backend/settings/base.py
@@ -42,7 +42,22 @@ TEMPLATES = [
         },
     },
 ]
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
 
+SITE_ID = 2
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LANGUAGE_CODE = 'pt-br'
@@ -80,7 +95,10 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 
 INSTALLED_APPS += ('admin_honeypot', 'bootstrap4', 'frontend', 'dashboard', 'perfil',
-                   'axes',  'crispy_forms', 'django.contrib.humanize', 'rest_framework',  'rest_registration',  'corsheaders',)
+                   'axes',  'crispy_forms', 'django.contrib.humanize',   'django.contrib.sites', 'rest_framework',  'rest_registration',  'corsheaders','allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',)
 
 MIDDLEWARE += ('axes.middleware.AxesMiddleware','corsheaders.middleware.CorsMiddleware',)
 
@@ -105,6 +123,7 @@ MESSAGE_TAGS = {
 AUTHENTICATION_BACKENDS = [
     'axes.backends.AxesBackend',
     'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 
