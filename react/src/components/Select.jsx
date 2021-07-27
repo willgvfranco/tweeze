745725a99831033607b36dbb105ca4e8a980bfee
Renan Oliveira <ren.moura.oliveira@gmail.com>
2021-07-27 00:53:06 -0300

    news and account pages

---
+++ react/src/components/Select.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select
+} from '@material-ui/core';
+
+const SelectForm = ({ className, labelId, id, value, onChange, label, style, items }) => (
+  <FormControl className={className} variant="outlined" style={style}>
+    <InputLabel id={labelId}>{label}</InputLabel>
+    <Select
+      labelId={labelId}
+      id={id}
+      value={value}
+      onChange={onChange}
+      label={label}
+    >
+      {items?.map((item, index) => (
+        <MenuItem key={`${index}-${item}`} value={item}>{item}</MenuItem>
+      )) || <MenuItem value="">
+          <em>Nenhum</em>
+        </MenuItem>}
+    </Select>
+  </FormControl>
+);
+
+export default SelectForm
