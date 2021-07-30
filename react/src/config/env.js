28f5b4bd9278f96413fc5e349cb8e42524de855c
William Franco <williamgvfranco@gmail.com>
2021-07-29 22:01:19 -0300

    react connected to prod

--- react/src/config/env.js
+++ react/src/config/env.js
@@ -1,4 +1,7 @@
-const ROOT_URL = 'http://localhost:7777';
+require('dotenv').config();
+
+// const ROOT_URL = 'http://localhost:7777';
+const ROOT_URL = 'http://165.227.72.240:7777';
 const BACKEND = {
   login: `${ROOT_URL}/api/auth/signin`,
   cadastro: `${ROOT_URL}/api/auth/signup`
