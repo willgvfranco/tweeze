104717e41848a7a47edc00f1fc52fb6493d840cb
Renan Oliveira <ren.moura.oliveira@gmail.com>
2021-08-17 23:43:01 -0300

    minha-conta refactor

--- frontend/src/views/MinhaConta/index.js
+++ frontend/src/views/MinhaConta/index.js
@@ -1,15 +1,68 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { makeStyles } from '@material-ui/core/styles';
 import { Card, Grid } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Settings } from '@material-ui/icons';
 
 import PageTitle from '../../components/PageTitle';
 
-import illustration1 from '../../assets/images/illustrations/pack1/analysis.svg';
-import illustration2 from '../../assets/images/illustrations/pack1/businessman.svg';
-import illustration3 from '../../assets/images/illustrations/pack1/authentication.svg';
+import AccountImg from '../../assets/images/illustrations/pack1/analysis.svg';
+import FinancialImg from '../../assets/images/illustrations/pack1/businessman.svg';
+import SecurityImg from '../../assets/images/illustrations/pack1/authentication.svg';
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    [theme.breakpoints.down('md')]: {
+      display: 'flex',
+      justifyContent: 'center'
+    }
+  },
+  title: {
+    [theme.breakpoints.down('md')]: {
+      justifyContent: 'center'
+    }
+  }
+}));
+
+const SettingsCard = ({ image, title, link }) => {
+  const classes = useStyles();
+
+  return (
+    <Card>
+      <div className="p-4">
+        <Grid container spacing={0}>
+          <Grid item md={3} className={classes.card}>
+            <img
+              alt="..."
+              className="img-fluid"
+              style={{ minHeight: '100px', maxHeight: '150px' }}
+              src={image}
+            />
+          </Grid>
+          <Grid
+            item
+            md={9}
+            className={`d-flex align-items-center ${classes.title}`}>
+            <div>
+              <div className="font-size-lg font-weight-bold mb-1 ml-3">
+                {title}
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+      <div className="divider" />
+      <NavLink
+        to={link}
+        className="px-4 py-3 text-first btn btn-white shadow-none d-flex justify-content-between align-items-center">
+        <div>Acessar</div>
+        <FontAwesomeIcon icon={['fas', 'chevron-right']} />
+      </NavLink>
+    </Card>
+  );
+};
 
 const MinhaConta = () => (
   <>
@@ -20,98 +73,30 @@ const MinhaConta = () => (
     />
 
     <Grid container spacing={2}>
-      <Grid item xs={6}>
-        <Card>
-          <div className="p-4">
-            <Grid container spacing={0}>
-              <Grid item md={3}>
-                <img
-                  alt="..."
-                  className="img-fluid"
-                  style={{ minHeight: '100px', maxHeight: '150px' }}
-                  src={illustration1}
-                />
-              </Grid>
-              <Grid item md={9} className="d-flex align-items-center">
-                <div>
-                  <div className="font-size-lg font-weight-bold mb-1 ml-3">
-                    Informações da Conta
-                  </div>
-                </div>
-              </Grid>
-            </Grid>
-          </div>
-          <div className="divider" />
-          <NavLink
-            to="/minha-conta/informacoes"
-            className="px-4 py-3 text-first btn btn-white shadow-none d-flex justify-content-between align-items-center">
-            <div>Acessar</div>
-            <FontAwesomeIcon icon={['fas', 'chevron-right']} />
-          </NavLink>
-        </Card>
+      <Grid item xs={12} md={6}>
+        <SettingsCard
+          image={AccountImg}
+          title="Informações da Conta"
+          link="/minha-conta/informacoes"
+        />
       </Grid>
-      <Grid item xs={6}>
-        <Card>
-          <div className="p-4">
-            <Grid container spacing={0}>
-              <Grid item md={3}>
-                <img
-                  alt="..."
-                  className="img-fluid"
-                  style={{ minHeight: '100px', maxHeight: '150px' }}
-                  src={illustration3}
-                />
-              </Grid>
-              <Grid item md={9} className="d-flex align-items-center">
-                <div>
-                  <div className="font-size-lg font-weight-bold mb-1 ml-3">
-                    Segurança
-                  </div>
-                </div>
-              </Grid>
-            </Grid>
-          </div>
-          <div className="divider" />
-          <NavLink
-            to="/minha-conta/seguranca"
-            className="px-4 py-3 text-first btn btn-white shadow-none d-flex justify-content-between align-items-center">
-            <div>Acessar</div>
-            <FontAwesomeIcon icon={['fas', 'chevron-right']} />
-          </NavLink>
-        </Card>
+
+      <Grid item xs={12} md={6}>
+        <SettingsCard
+          image={SecurityImg}
+          title="Segurança"
+          link="/minha-conta/seguranca"
+        />
       </Grid>
     </Grid>
 
     <Grid container spacing={2} className="mt-3">
-      <Grid item xs={6}>
-        <Card>
-          <div className="p-4">
-            <Grid container spacing={0}>
-              <Grid item md={3}>
-                <img
-                  alt="..."
-                  className="img-fluid"
-                  style={{ minHeight: '100px', maxHeight: '150px' }}
-                  src={illustration2}
-                />
-              </Grid>
-              <Grid item md={9} className="d-flex align-items-center">
-                <div>
-                  <div className="font-size-lg font-weight-bold mb-1 ml-3">
-                    Dados Financeiros
-                  </div>
-                </div>
-              </Grid>
-            </Grid>
-          </div>
-          <div className="divider" />
-          <NavLink
-            to="/minha-conta/financeiro"
-            className="px-4 py-3 text-first btn btn-white shadow-none d-flex justify-content-between align-items-center">
-            <div>Acessar</div>
-            <FontAwesomeIcon icon={['fas', 'chevron-right']} />
-          </NavLink>
-        </Card>
+      <Grid item xs={12} md={6}>
+        <SettingsCard
+          image={FinancialImg}
+          title="Dados Financeiros"
+          link="/minha-conta/financeiro"
+        />
       </Grid>
     </Grid>
   </>
