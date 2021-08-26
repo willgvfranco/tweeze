07c70997246d493a88f631f71b161858e515c89d
William Franco <williamgvfranco@gmail.com>
2021-08-25 22:18:17 -0300

    new endpoint for create session

--- api/controllers/payment.controller.js
+++ api/controllers/payment.controller.js
@@ -1,31 +1,23 @@
-// // SDK do Mercado Pago
-// const mercadopago = require("mercadopago");
-// // Adicione as credenciais
-// mercadopago.configure({
-//   access_token:
-//     "TEST-6509143103427068-081701-92a51eef0ec7eb13b048be86261f47c5-180597816",
-// });
+import axios from "axios";
 
-// // Cria um objeto de preferência
-// let preference = {
-//   items: [
-//     {
-//       title: "Tweeze",
-//       unit_price: 49.9,
-//       quantity: 1,
-//     },
-//   ],
-// };
+export async function createSession(req, res) {
+  const email = "williamgvfranco@gmail.com";
+  const pvtToken =
+    "5ed39c32-abba-41e1-a3ad-cf068d63e200f0df0d5541f4b7db605ed8b78bf3472a56b7-76f4-495c-a5a3-e13ca5d0786c";
 
-// mercadopago.preferences
-//   .create(preference)
-//   .then(function (response) {
-//     // Este valor substituirá a string "<%= global.id %>" no seu HTML
-//     global.id = response.body.id;
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
+  const url = `https://ws.pagseguro.uol.com.br/v2/sessions?email=${email}&token=${pvtToken}`;
+
+  axios.get(url).then((response) => {
+    console.log(response);
+  });
+}
+
+export async function signPlan(req, res) {}
+
+export async function getCardToken(req, res) {}
+
+export async function checkPayment(req, res) {}
+
 export async function newPayment(req, res) {
   console.log(req.body);
   res.status(200).send({ message: "ok" });
